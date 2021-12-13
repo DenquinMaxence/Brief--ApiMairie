4c5e8d2dcfcd52d8771338b3663248446f384419
DenquinMaxence <denquin.maxence.pro@gmail.com>
2021-12-13 19:41:26 +0100

    new: sendEmail WIP

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,5 +1,6 @@
 import reportModel from '../models/reportModel.js';
 import { StatusCodes } from 'http-status-codes';
+import sendEmail from '../utils/sendEmail.js';
 
 export const selectAllReports = async (req, res) => {
 	const reports = await reportModel.find();
@@ -21,6 +22,49 @@ export const createReport = async (req, res) => {
 		postalSender,
 		phoneSender,
 	} = req.body;
+
+	let sendTo;
+	try {
+		if (typeReport === 'highways') sendTo = 'voirie@simplonville.co';
+		else if (typeReport === 'parking') sendTo = 'stationnement@simplonville.co';
+		else if (typeReport === 'works') sendTo = 'travaux@simplonville.co';
+		else if (typeReport === 'animals') sendTo = 'animaux@simplonville.co';
+		else sendTo = 'autres@simplonville.co';
+
+		console.log(sendTo, dateReport);
+
+		sendEmail(
+			sendTo,
+			'Nouveau signalement',
+			`
+			<div>
+				<h1>Informations sur le signalement</h1>
+				<p>Description: ${descReport}</p>
+				<p>Date et heure: ${dateReport}</p>
+				<p>Adresse: ${addressReport}</p>
+				<p>Photo:
+					${
+						pictureReport
+							? `<a href="${pictureReport}">Cliquer ici pour voir l'image</a>`
+							: 'Aucune photo fourni'
+					}
+				</p>
+			</div>
+			<div>
+				<h1>Informations sur l'expéditeur du signalement</h1>
+				<p>Nom: ${lastNameSender}</p>
+				<p>Prénom: ${firstNameSender}</p>
+				<p>Email: ${emailSender}</p>
+				<p>Adresse: ${addressSender}</p>
+				<p>Ville: ${citySender}</p>
+				<p>Code postal: ${postalSender}</p>
+				<p>Numéro de téléphone: ${phoneSender}</p>
+			</div>
+		`
+		);
+	} catch (error) {
+		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error.message });
+	}
 
 	try {
 		const report = await reportModel.create({
