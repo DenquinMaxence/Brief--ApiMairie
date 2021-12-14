0501a6c427a750df15927b2a05c87bacd038d8ac
DenquinMaxence <denquin.maxence.pro@gmail.com>
2021-12-14 11:52:13 +0100

    remove: selectAllReports function removed from controller & route

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import checkBodyReport from '../utils/checkBodyReport.js';
 import { uploadPicture } from '../middleware/cloudinary.js';
-import { selectAllReports, createReport } from '../controllers/reportController.js';
+import { createReport } from '../controllers/reportController.js';
 
 const router = Router();
 
-router.get('/', selectAllReports).post('/', uploadPicture, checkBodyReport, createReport);
+router.post('/', uploadPicture, checkBodyReport, createReport);
 
 export default router;
