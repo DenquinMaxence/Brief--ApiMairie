f5b5bec40d99fac446d5d7985f268c2ed83c5ee7
DenquinMaxence <denquin.maxence.pro@gmail.com>
2021-12-20 23:42:00 +0100

    new: checkBodySignUp middleware added for register route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 const router = Router();
+import checkBodySignUp from '../utils/checkBodySignUp.js';
 import { signUp, signIn, signOut } from '../controllers/authController.js';
 import verifyToken from '../middleware/authMiddleware.js';
 
 // Sign up
-router.post('/register', signUp);
+router.post('/register', checkBodySignUp, signUp);
 
 // Sign in
 router.post('/login', signIn);
