3f0677db36ea8d8c94d8e917659bdc48c8a55d54
DenquinMaxence <denquin.maxence.pro@gmail.com>
2021-12-21 12:34:17 +0100

    change: refacto

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,16 +4,12 @@ import checkBodySignUp from '../middleware/checkBodySignUp.js';
 import { signUp, signIn, getMe, signOut } from '../controllers/authController.js';
 import verifyToken from '../middleware/authMiddleware.js';
 
-// Sign up
+// Auth user
 router.post('/register', checkBodySignUp, signUp);
-
-// Sign in
 router.post('/login', signIn);
+router.get('/logout', verifyToken, signOut);
 
 // Get user info
 router.post('/me', verifyToken, getMe);
-
-// Sign out
-router.get('/logout', verifyToken, signOut);
 
 export default router;
