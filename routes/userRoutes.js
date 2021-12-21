4ea816b79c9df53a3fc30ec156ecd30c84c654ef
DenquinMaxence <denquin.maxence.pro@gmail.com>
2021-12-21 16:45:54 +0100

    new: route for connected user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,11 +6,16 @@ import {
 	deleteUser,
 	getSingleUser,
 } from '../controllers/userController.js';
+import verifyToken from '../middleware/authMiddleware.js';
 
 // Manage user
 router.get('/', getAllUsers); // Get all users (admin only)
 router.get('/:id', getSingleUser); // Get user (admin only)
+
+router.put('/', verifyToken, updateUser); // Update user
 router.put('/:id', updateUser); // Update user (admin only)
+
+router.delete('/', verifyToken, deleteUser); // Delete user (admin only)
 router.delete('/:id', deleteUser); // Delete user (admin only)
 
 export default router;
