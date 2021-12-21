969c58e44399d2ec33eb6c9e3653b74781ea9a22
DenquinMaxence <denquin.maxence.pro@gmail.com>
2021-12-21 10:07:43 +0100

    new: post route on "/me"

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 const router = Router();
 import checkBodySignUp from '../middleware/checkBodySignUp.js';
-import { signUp, signIn, signOut } from '../controllers/authController.js';
+import { signUp, signIn, getMe, signOut } from '../controllers/authController.js';
 import verifyToken from '../middleware/authMiddleware.js';
 
 // Sign up
@@ -9,6 +9,9 @@ router.post('/register', checkBodySignUp, signUp);
 
 // Sign in
 router.post('/login', signIn);
+
+// Get user info
+router.post('/me', verifyToken, getMe);
 
 // Sign out
 router.get('/logout', verifyToken, signOut);
