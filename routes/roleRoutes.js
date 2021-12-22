9f93e25cd19ff9ca30c60a5db9ec3240f11d42aa
DenquinMaxence <denquin.maxence.pro@gmail.com>
2021-12-22 12:50:15 +0100

    new: getSingleRole function

--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 const router = Router();
 import checkBodyRole from '../middleware/checkBodyRole.js';
-import { getAllRoles, createRole, updateRole, deleteRole } from '../controllers/roleController.js';
+import {
+	getAllRoles,
+	createRole,
+	updateRole,
+	deleteRole,
+	getSingleRole,
+} from '../controllers/roleController.js';
 
 // api/v1/role/
 router.get('/', getAllRoles).post('/', checkBodyRole, createRole);
 
 // api/v1/role/:id
-router.put('/:id', checkBodyRole, updateRole).delete('/:id', deleteRole);
+router.get('/:id', getSingleRole).put('/:id', checkBodyRole, updateRole).delete('/:id', deleteRole);
 
 export default router;
