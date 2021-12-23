cb5f56fc82b36989979ed73db0eb915f812484f7
DenquinMaxence <denquin.maxence.pro@gmail.com>
2021-12-23 11:13:29 +0100

    change: now check is Authenticated for logout & me routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,13 +3,14 @@ const router = Router();
 import checkBodySignUp from '../middleware/checkBodySignUp.js';
 import { signUp, signIn, getMe, signOut } from '../controllers/authController.js';
 import verifyToken from '../middleware/authMiddleware.js';
+import isAuthenticated from '../utils/isAuthenticated.js';
 
 // Auth user
 router.post('/register', checkBodySignUp, signUp);
 router.post('/login', signIn);
-router.get('/logout', verifyToken, signOut);
+router.get('/logout', verifyToken, isAuthenticated, signOut);
 
 // Get user info
-router.post('/me', verifyToken, getMe);
+router.post('/me', verifyToken, isAuthenticated, getMe);
 
 export default router;
